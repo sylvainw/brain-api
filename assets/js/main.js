803967131c87f1c1bd10603f0d4c5987eaf1d97a
sylvainw <sylvain.weber@gmail.com>
2014-04-26 22:22:55 +0200

    First version at the end of the Hackathon : Beta Showcase + Doc automated system

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+$(function() {
+  $( ".open-disqus" ).click(function() {
+	  $( "#disqus_thread" ).show();
+  });
+});
---
+++ doc/api_project.js
@@ -0,0 +1,10 @@
+define({
+  "name": "example-basic",
+  "version": "0.1.0",
+  "description": "A apiDoc basic example",
+  "apidoc": "",
+  "generator": {
+    "version": "0.4.2",
+    "time": "2014-04-26T13:50:47.762Z"
+  }
+});
